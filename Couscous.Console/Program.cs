423ac4f4d6bd47ea61defeb9054af965c2c0151e
sam <habtard@gmail.com>
2020-02-15 21:47:43 +0000

    Initial commit

---
+++ Couscous.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Couscous.Console
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
