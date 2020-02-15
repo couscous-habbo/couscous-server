a9d351f1b307a4805b72dc74611258a9922545f6
sam <habtard@gmail.com>
2020-02-15 21:49:52 +0000

    Console now stays alive?

--- Couscous.Console/Program.cs
+++ Couscous.Console/Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
-
-namespace Couscous.Console
+﻿namespace Couscous.Console
 {
     internal static class Program
     {
         private static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+                System.Console.ReadLine(); 
+            }
         }
     }
 }
