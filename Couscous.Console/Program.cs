52f07f55ab78b44e49afff6c5fc6d4325ac90546
sam <habtard@gmail.com>
2020-02-17 17:13:02 +0000

    Networking is now reading binary data.

--- Couscous.Console/Program.cs
+++ Couscous.Console/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Couscous.Console
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using Couscous.Networking;
+
+namespace Couscous.Console
 {
     internal static class Program
     {
         private static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World!");
+
+            var networkHandler = new NetworkHandler(
+                new TcpListener(IPAddress.Any, 1232),
+                new List<NetworkClient>()
+            );
+            
+            networkHandler.ListenAsync();
 
             while (true)
             {
--- Couscous.Networking/NetworkClient.cs
+++ Couscous.Networking/NetworkClient.cs
@@ -1,25 +1,53 @@
 using System;
+using System.IO;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Couscous.Networking
 {
     public class NetworkClient : IDisposable
     {
-        private readonly TcpClient _client;
+        private readonly TcpClient _tcpClient;
         
-        public NetworkClient(TcpClient client)
+        public NetworkClient(TcpClient tcpClient)
         {
-            _client = client;
+            _tcpClient = tcpClient;
         }
 
-        public void StartReceiving()
+        public async void StartReceiving()
         {
-            
+            await ProcessDataAsync();
+        }
+
+        private async Task ProcessDataAsync()
+        {
+            while (true)
+            {
+                var binaryData = await GetBinaryDataAsync();
+                Console.WriteLine(Encoding.Default.GetString(binaryData));
+            }
+        }
+        
+        private async Task<byte[]> GetBinaryDataAsync()
+        {
+            var buffer = new byte[2048];
+            var memoryStream = new MemoryStream();
+        
+            var bytesRead = await _tcpClient.GetStream().ReadAsync(buffer, 0, buffer.Length);
+
+            while (bytesRead > 0)
+            {
+                memoryStream.Write(buffer, 0, buffer.Length);
+                bytesRead = await memoryStream.ReadAsync(buffer, 0, buffer.Length);
+            }
+
+            return memoryStream.ToArray();
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _tcpClient.Dispose();
         }
     }
 }
