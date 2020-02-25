6b2fbf703c2d862acf62b4c2417a59e5287eac58
sam <habtard@gmail.com>
2020-02-25 07:08:21 +0000

    Progressed with database logic.

--- Couscous.Database/DatabaseConnection.cs
+++ Couscous.Database/DatabaseConnection.cs
@@ -1,7 +1,18 @@
+using MySql.Data.MySqlClient;
+
 namespace Couscous.Database
 {
     public class DatabaseConnection
     {
-        
+        private readonly MySqlConnection _connection;
+        private readonly MySqlCommand _command;
+
+        public DatabaseConnection(MySqlConnection connection, MySqlCommand command)
+        {
+            _connection = connection;    
+            _command = command;
+            
+            _connection.Open();
+        }
     }
 }
--- Couscous.Database/DatabaseConnectionProvider.cs
+++ Couscous.Database/DatabaseConnectionProvider.cs
@@ -1,7 +1,35 @@
+using MySql.Data.MySqlClient;
+
 namespace Couscous.Database
 {
     public class DatabaseConnectionProvider
     {
+        private readonly string _connectionString;
         
+        public DatabaseConnectionProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        public DatabaseConnection GetConnection()
+        {
+            var connection = new MySqlConnection(_connectionString);
+            var command = connection.CreateCommand();
+            
+            return new DatabaseConnection(connection, command);
+        }
+
+        public bool IsConnected()
+        {
+            try
+            {
+                GetConnection();
+                return true;
+            }
+            catch (MySqlException)
+            {
+                return false;
+            }
+        }
     }
 }
