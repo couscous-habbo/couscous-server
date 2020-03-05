d99f37c4b20aff0c5d470d7afcd8edd6cd6756b4
sam <habtard@gmail.com>
2020-03-05 12:46:34 +0000

    Renamed PlayerHandler to use correct file name.

--- Couscous.Game/Players/PlayerHandler.cs
+++ Couscous.Game/Players/PlayerHandler.cs
@@ -6,12 +6,10 @@ namespace Couscous.Game.Players
     public class PlayerHandler
     {
         private readonly PlayerRepository _playerRepository;
-        private readonly IDictionary<int, Player> _players;
 
         public PlayerHandler(PlayerRepository playerRepository)
         {
             _playerRepository = playerRepository;
-            _players = new Dictionary<int, Player>();
         }
 
         public async Task<Player> GetPlayerBySsoTicketAsync(string ssoTicket)
