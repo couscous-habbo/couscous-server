a6f208b699070cc8867af6b718fcb0f09b5209d7
sam <habtard@gmail.com>
2020-03-10 15:14:40 +0000

    Progressed with dependency injection.

--- Couscous.Console/DependencyProvider.cs
+++ Couscous.Console/DependencyProvider.cs
@@ -1,4 +1,16 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using Couscous.Config;
+using Couscous.Database;
+using Couscous.Game;
+using Couscous.Game.Players;
+using Couscous.Networking;
+using Couscous.Networking.Packets.Client;
+using Couscous.Networking.Packets.Client.Handshake;
+using Couscous.Networking.Packets.Client.Tracking;
 using Microsoft.Extensions.DependencyInjection;
+using MySql.Data.MySqlClient;
 
 namespace Couscous.Console
 {
@@ -9,7 +21,54 @@ namespace Couscous.Console
         /// </summary>
         public void Register()
         {
+            this.AddSingleton<IConfigProvider, RemoteJsonConfigProvider>();
+
+            var serviceProvider = this.BuildServiceProvider();
+            var configProvider = serviceProvider.GetService<IConfigProvider>();
             
+            configProvider.Load("http://tiny.cc/zo6gkz"); // cba :(
+            
+            var connectionString = new MySqlConnectionStringBuilder
+            {
+                Database = configProvider.GetValueFromKey("database.name"),
+                Password = configProvider.GetValueFromKey("database.password"),
+                Port = uint.Parse(configProvider.GetValueFromKey("database.port")),
+                Server = configProvider.GetValueFromKey("database.host"),
+                UserID = configProvider.GetValueFromKey("database.username"),
+                SslMode = MySqlSslMode.None
+            }.ToString();
+            
+            var databaseProvider = new DatabaseProvider(connectionString);
+            
+            var playerDao = new PlayerDao(databaseProvider);
+            var playerRepository = new PlayerRepository(playerDao);
+            var playerProvider = new PlayerHandler(playerRepository);
+            var gameProvider = new GameProvider(playerProvider);
+            
+            var packets = new Dictionary<int, IClientPacket>
+            {
+                { ClientPacketId.SendPolicyFileRequest, new SendPolicyFilePacket() },
+                { ClientPacketId.ReceiveClientVersion, new ReceivedClientVersionPacket() },
+                { ClientPacketId.RequestEncryptionKeys, new RequestEncryptionKeysPacket() },
+                { ClientPacketId.ReceiveUniqueMachineId, new ReceivedUniqueMachineIdPacket() },
+                { ClientPacketId.PerformanceLog, new PerformanceLogPacket() },
+                { ClientPacketId.SecureLogin, new SecureLoginPacket(playerProvider) }
+            };
+
+            var packetProvider = new ClientPacketProvider(packets);
+
+            var networkHandler = new NetworkHandler(
+                new List<NetworkClient>(),
+                packetProvider
+            );
+            
+            var networkListener = new NetworkListener(
+                new TcpListener(IPAddress.Any, int.Parse(configProvider.GetValueFromKey("networking.port"))),
+                networkHandler
+            );
+
+            var server = new Server(networkListener, gameProvider);
+            server.Start();
         }
     }
 }
--- Couscous.Console/Program.cs
+++ Couscous.Console/Program.cs
@@ -1,17 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Net;
-using System.Net.Sockets;
-using Couscous.Config;
-using Couscous.Database;
-using Couscous.Game;
-using Couscous.Game.Players;
-using Couscous.Networking;
-using Couscous.Networking.Packets.Client;
-using Couscous.Networking.Packets.Client.Handshake;
-using Couscous.Networking.Packets.Client.Tracking;
-using MySql.Data.MySqlClient;
-
-namespace Couscous.Console
+﻿namespace Couscous.Console
 {
     internal static class Program
     {
@@ -19,52 +6,6 @@ namespace Couscous.Console
         {
             var dependencyProvider = new DependencyProvider();
             dependencyProvider.Register();
-            
-            var configProvider = new RemoteJsonConfigProvider();
-            
-            configProvider.Load("http://tiny.cc/zo6gkz"); // cba :(
-            
-            var connectionString = new MySqlConnectionStringBuilder
-            {
-                Database = configProvider.GetValueFromKey("database.name"),
-                Password = configProvider.GetValueFromKey("database.password"),
-                Port = uint.Parse(configProvider.GetValueFromKey("database.port")),
-                Server = configProvider.GetValueFromKey("database.host"),
-                UserID = configProvider.GetValueFromKey("database.username"),
-                SslMode = MySqlSslMode.None
-            }.ToString();
-            
-            var databaseProvider = new DatabaseProvider(connectionString);
-            
-            var playerDao = new PlayerDao(databaseProvider);
-            var playerRepository = new PlayerRepository(playerDao);
-            var playerProvider = new PlayerHandler(playerRepository);
-            var gameProvider = new GameProvider(playerProvider);
-            
-            var packets = new Dictionary<int, IClientPacket>
-            {
-                { ClientPacketId.SendPolicyFileRequest, new SendPolicyFilePacket() },
-                { ClientPacketId.ReceiveClientVersion, new ReceivedClientVersionPacket() },
-                { ClientPacketId.RequestEncryptionKeys, new RequestEncryptionKeysPacket() },
-                { ClientPacketId.ReceiveUniqueMachineId, new ReceivedUniqueMachineIdPacket() },
-                { ClientPacketId.PerformanceLog, new PerformanceLogPacket() },
-                { ClientPacketId.SecureLogin, new SecureLoginPacket(playerProvider) }
-            };
-
-            var packetProvider = new ClientPacketProvider(packets);
-
-            var networkHandler = new NetworkHandler(
-                new List<NetworkClient>(),
-                packetProvider
-            );
-            
-            var networkListener = new NetworkListener(
-                new TcpListener(IPAddress.Any, int.Parse(configProvider.GetValueFromKey("networking.port"))),
-                networkHandler
-            );
-
-            var server = new Server(networkListener, gameProvider);
-            server.Start();
 
             while (true)
             {
