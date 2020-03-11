1468665d3b74be76176c18ab5fe11c34e1e6f2a4
sam <habtard@gmail.com>
2020-03-11 12:16:26 +0000

    We're now validating the database connection properly.

--- Couscous.Console/Server.cs
+++ Couscous.Console/Server.cs
@@ -1,4 +1,5 @@
 using System;
+using Couscous.Database;
 using Couscous.Game;
 using Couscous.Logging;
 using Couscous.Networking;
@@ -8,26 +9,44 @@ namespace Couscous.Console
     public class Server : IDisposable
     {
         private readonly ILogger _logger;
+        private readonly IDatabaseProvider _databaseProvider;
         private readonly NetworkListener _networkListener;
         private readonly GameProvider _gameProvider;
         
-        public Server(ILogger logger, NetworkListener networkListener, GameProvider gameProvider)
+        public Server(ILogger logger, IDatabaseProvider databaseProvider, NetworkListener networkListener, GameProvider gameProvider)
         {
             _logger = logger;
+            _databaseProvider = databaseProvider;
             _networkListener = networkListener;
             _gameProvider = gameProvider;
         }
 
         public void Start()
         {
+            if (_databaseProvider.IsConnected())
+            {
+                _logger.Error("Failed to connect to the database, press any key to exit.");
+                ExitAfterKey();
+            }
+            
             _networkListener.StartListener();
             _networkListener.ListenAsync();
             
             _logger.Success("Server has finished starting.");
         }
 
+        private void ExitAfterKey()
+        {
+            System.Console.ReadKey(true);
+            Dispose();
+            
+            Environment.Exit(-1);
+        }
+
         public void Dispose()
         {
+            _logger.Warning("Please wait, the server is disposing...");
+            
             _networkListener.Dispose();
             _gameProvider.Dispose();
         }
